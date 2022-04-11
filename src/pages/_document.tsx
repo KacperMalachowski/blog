d53eff9a8bea508f409416c2e1a483ce72463fc5
Kacper Małachowski <kacpermaachowski@gmail.com>
2022-04-11 21:26:58 +0200

    admitad verify

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="verify-admitad" content="871e023e39" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
