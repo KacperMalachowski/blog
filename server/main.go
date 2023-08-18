ad331ae3d7fb1d1ddf232897caa46eab14e402a7
Kacper Malachowski <kacpermaachowski@gmail.com>
2023-08-18 09:41:52 +0200

    Prepare to run on cloud run

--- server/main.go
+++ server/main.go
@@ -12,8 +12,8 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", Hello)
 	http.Handle("/", r)
-	fmt.Println("Starting up on 3000")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	fmt.Println("Starting up on 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func Hello(w http.ResponseWriter, req *http.Request) {
