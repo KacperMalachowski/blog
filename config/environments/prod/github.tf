0e8c4c0c21bb77e88da747538cd13df2def6ee17
Kacper Malachowski <kacpermaachowski@gmail.com>
2024-12-18 21:02:29 +0100

    Remove unused workflows

--- config/environments/prod/github.tf
+++
@@ -1,9 +0,0 @@
-data "github_repository" "blog" {
-  name = var.github_blog_repository_name
-}
-
-resource "github_actions_variable" "gcp_blog_project_id" {
-  repository    = data.github_repository.blog.name
-  variable_name = "GCP_BLOG_PROJECT_ID"
-  value         = var.gcp_project_id
-}
