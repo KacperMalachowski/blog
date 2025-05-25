152cad11ac25c018e7b3072232089db20703b712
Kacper Małachowski <kacpermaachowski@gmail.com>
2025-05-25 10:35:53 +0200

    Setup strapi

---
+++ cms/config/plugins.ts
@@ -0,0 +1,23 @@
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        credentials: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+        },
+        region: env("AWS_REGION"),
+        endpoint: env("AWS_S3_ENDPOINT"),
+        params: {
+          Bucket: env("AWS_S3_BUCKET"),
+        },
+      },
+    },
+    actionOptions: {
+      upload: {},
+      uploadStream: {},
+      delete: {},
+    },
+  },
+});
